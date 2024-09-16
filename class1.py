69c651200daccda419bdf83afc3a5187350c55d2
Praveen P <107240891+Praveenp2508@users.noreply.github.com>
2024-09-16 10:44:55 +0530

    Update class1.py

--- class1.py
+++ class1.py
@@ -1,13 +1,13 @@
-# a = int(input('enter number :'))
-# b = int(input('enter number :'))
-#
-# c = a + b
-# print(c)
-#
-# length = int(input('enter the length'))
-# breadth=int(input('enter the breadth'))
-# area= length*breadth
-# print(area)
+a = int(input('enter number :'))
+b = int(input('enter number :'))
+
+c = a + b
+print(c)
+
+length = int(input('enter the length'))
+breadth=int(input('enter the breadth'))
+area= length*breadth
+print(area)
 
 import math as m
 radius = int(input('enter the radius :'))
