93b6c4f7318b2ae8d34618962681aa790de7f7c6
Praveen P <107240891+Praveenp2508@users.noreply.github.com>
2024-09-16 10:44:13 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,15 @@
+# a = int(input('enter number :'))
+# b = int(input('enter number :'))
+#
+# c = a + b
+# print(c)
+#
+# length = int(input('enter the length'))
+# breadth=int(input('enter the breadth'))
+# area= length*breadth
+# print(area)
+
+import math as m
+radius = int(input('enter the radius :'))
+area = round(m.pi,3)*(radius**2)
+print(area)
